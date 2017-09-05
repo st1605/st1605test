02e1247738de4de958d83d34256067c660212b16
shenxiang <shenxiang@DESKTOP-4FEI77Q>
2017-09-05 16:53:27 +0800

    eeee

---
+++ t/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+
+import com.bean.User;
+
+@Component
+public class UserDao {
+	
+	private SessionFactory sessionFactory;
+	
+	public void saveUser(User user){
+		
+	}
+
+}
